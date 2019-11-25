1598ef92a99f528ae5896f6cf9aea6bb22e28950
Kyle <arow411@gmail.com>
2019-11-25 10:53:32 -0500

    Removed webhooks in favor of embeds, added channel separation

--- app.js
+++ app.js
@@ -13,36 +13,10 @@ const mcClient = mc.createClient({
     version: config.mcVersion
 })
 
-let webhooks = {}
-let channel
+let discordChannel
 discordClient.on('ready', async () => {
     discordClient.user.setActivity(`${config.mcHost}${config.mcPort===25565?'':':'+config.mcPort}`)
-    channel = discordClient.channels.get(config.botchannel)
-    var whfile = path.join(__dirname, 'config', 'webhooks.json')
-    var wh = {}
-    if(fs.existsSync(whfile)) {
-        wh = JSON.parse(fs.readFileSync(whfile))
-    }
-    if(wh.primaryId && wh.primaryToken && wh.secondaryId && wh.secondaryToken) {
-        webhooks.primary = new Discord.WebhookClient(wh.primaryId, wh.primaryToken)
-        webhooks.secondary = new Discord.WebhookClient(wh.secondaryId, wh.secondaryToken)
-    } else {
-        try {
-            webhooks.primary = await channel.createWebhook('primary webhook', 'https://crafatar.com/renders/head/8667ba71b85a4004af54457a9734eed7')
-            webhooks.primary.edit('primary webhook', 'https://crafatar.com/renders/head/8667ba71b85a4004af54457a9734eed7')
-            webhooks.secondary = await channel.createWebhook('secondary webhook', 'https://crafatar.com/renders/head/8667ba71b85a4004af54457a9734eed7')
-            webhooks.secondary.edit('secondary webhook', 'https://crafatar.com/renders/head/8667ba71b85a4004af54457a9734eed7')
-        } catch(e) {
-            console.error(e)
-        }
-        wh = {
-            primaryId: webhooks.primary.id,
-            primaryToken: webhooks.primary.token,
-            secondaryId: webhooks.secondary.id,
-            secondaryToken: webhooks.secondary.token,
-        }
-        fs.writeFileSync(whfile, JSON.stringify(wh))
-    }
+    discordChannel = discordClient.channels.get(config.botchannel[""])
     console.log(`Logged in to Discord as ${discordClient.user.tag}!`)
 })
 
@@ -82,31 +56,27 @@ function truncate(str, len) {
 }
 
 async function sendSnitchMessage(user, action, snitchName, worldName, x, y, z) {
-    await channel.send(`\`${user} ${action} ${snitchName} at ${x}, ${y}, ${z}\``)
+    await discordChannel.send(`\`${user} ${action} ${snitchName} at ${x}, ${y}, ${z}\``)
 }
 
-let usePrimary = false
-let lastUsedName = ''
 async function sendChatMessage(channel, username, message) {
     var name = username
     if(channel != null) {
         name = `[${channel}] ${name}`
     }
-    if(lastUsedName != name)
-        usePrimary = !usePrimary
-    lastUsedName = name
-
-    let webhook
-    if(usePrimary)
-        webhook = webhooks.primary
-    else
-        webhook = webhooks.secondary
+    const chatEmbed = new Discord.RichEmbed()
+        .setAuthor(name, `https://crafatar.com/renders/head/8667ba71b85a4004af54457a9734eed7`)
+        .setDescription(message)
+        .setTimestamp()
     
-    try {
-        await webhook.edit(truncate(name, 32), `https://crafatar.com/renders/head/8667ba71b85a4004af54457a9734eed7`)
-        await webhook.send(message)
-    } catch(e) {
-        console.error(e)
+    if(channel != null) {
+        chatEmbed.setFooter(channel)
+    }
+
+    if(config.botchannel[channel] != "") {
+        discordClient.channels.get(config.botchannel[channel]).send(chatEmbed)
+    } else {
+        discordChannel.send(chatEmbed)
     }
 }
 
